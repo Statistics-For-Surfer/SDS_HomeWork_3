b579cbc2a5a21a49cac8200b7735fa4c2a7dc623
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-02-07 11:55:25 +0100

    adding col

--- data.R
+++ data.R
@@ -1,20 +1,16 @@
+rm(list = ls())
 load("hw3_data.RData")
 
 # Count how many patients in the ASD group have time series of a given length
 sort( table( sapply(asd_data, function(x) nrow(x)) ), decreasing = T )
 
-
-
-
-
-
 library(manipulate)
 
-manipulate(plot(asd_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region),
+manipulate(plot(asd_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region), col = "skyblue", lwd = 2, 
                 xlab = 'observations', ylab = 'value'),
            patient = slider(1,length(asd_data)), region = slider(1,116))
 
 
-manipulate(plot(td_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region),
+manipulate(plot(td_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region), col = "darkred" , lwd = 2 ,  
                 xlab = 'observations', ylab = 'value'),
            patient = slider(1,length(td_data)), region = slider(1,116))
