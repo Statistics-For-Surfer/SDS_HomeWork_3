3e2b32eddbf66a85eada3eea8060a3b480e170e3
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-02-07 13:32:01 +0100

    add a prova

--- data.R
+++ data.R
@@ -14,3 +14,35 @@ manipulate(plot(asd_data[[patient]][[region]], type='l', main = paste0("Patient-
 manipulate(plot(td_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region), col = "darkred" , lwd = 2 ,  
                 xlab = 'observations', ylab = 'value'),
            patient = slider(1,length(td_data)), region = slider(1,116))
+
+##### Esercise 3
+
+
+# Get a sample from a specific distribution
+n <- 80
+m <- 90
+mu = 10
+sigma = 2
+
+
+x <- rnorm(n , mu , sigma)
+y_x <- rep(1, n)
+y_z <- rep(0, m)
+u <- c(y_x ,y_z)
+
+P = 1000
+
+t <- c()
+for (i in 1:P){
+  z <-rnorm(m , mu , sigma)
+  
+  
+} 
+
+
+length(u)
+?glm
+
+glm(u ~  x + z )
+
+
