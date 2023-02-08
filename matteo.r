edf0ea7d5245a875e046949c467fca3c71278a10
Matteo Candi <114286004+Matteo-Candi@users.noreply.github.com>
2023-02-08 11:13:50 +0100

    Update matteo.r

--- matteo.r
+++ matteo.r
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
 rm(list=ls())
 
-=======
-  rm(list = ls())
->>>>>>> 3e2b32eddbf66a85eada3eea8060a3b480e170e3
 load("hw3_data.RData")
 
 
@@ -31,7 +27,6 @@ asd_scale <- scale_datasets_list(asd_data)
 # Frequency of number observation  ----------------------------------------
 
 # Count how many patients in the ASD group have time series of a given length
-<<<<<<< HEAD
 sort( table( sapply(td_scale, function(x) nrow(x)) ), decreasing = T )
 sort( table( sapply(asd_scale, function(x) nrow(x)) ), decreasing = T )
 
@@ -39,10 +34,7 @@ sort( table( sapply(asd_scale, function(x) nrow(x)) ), decreasing = T )
 
 
 # Plot of time series of each ROI foreach patient -------------------------
-=======
-  sort( table( sapply(asd_data, function(x) nrow(x)) ), decreasing = T )
 
->>>>>>> 3e2b32eddbf66a85eada3eea8060a3b480e170e3
 library(manipulate)
 
 manipulate(plot(asd_data[[patient]][[region]], type='l', main = paste0("Patient-", patient, '\nROI-', region), col = "skyblue", lwd = 2, 
@@ -54,31 +46,24 @@ manipulate(plot(td_data[[patient]][[region]], type='l', main = paste0("Patient-"
                 xlab = 'observations', ylab = 'value'),
            patient = slider(1,length(td_data)), region = slider(1,116))
 
-<<<<<<< HEAD
 
 
 length(asd_data)
 
 # Take summaries of ROI's patients -----------------------------------------
 
-a <- function(list, fun){
-  # if(fun == 'mean') {fun <- mean}
-  # else if(fun == 'sd') {fun <- sd}
-  # else if(fun == 'median') {fun <- median}
-  # fun <- fun
-  tab <- matrix(NA, nrow = length(list), ncol = 116)
-  
-  len <- length(list)
-  
-  for(i in 1:len){
-    print(lapply(list[i], mean))
-    # print(unname(unlist(lapply(list[i], fun))))
-    tab[i,] <- unname(unlist(lapply(list[i], fun)))
-  }
-  
-  # return(tab)
-}
-
-a(td_scale, 'mean')
-
-o <- unname(unlist(lapply(td_scale$caltech_0051478, mean)))
+# a <- function(list, fun){
+#   len <- length(list)
+#   tab <- matrix(NA, nrow = 0, ncol = 116)
+#   
+#   for(i in 1:len){
+#     print((sapply(list[i], mean)))
+#     tab <- rbind(tab, unname(sapply(list[i], fun)))
+#   }
+#   
+#   # return(tab)
+# }
+# 
+# a(td_data, mean)
+# 
+# o <- unname(sapply(td_scale$caltech_0051478, mean))
