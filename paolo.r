ff78d7075b3ebdd0e57b4794807bb2515a960fab
PaoloBarba <114430394+PaoloBarba@users.noreply.github.com>
2023-02-16 16:27:07 +0100

    add studd

--- paolo.r
+++ paolo.r
@@ -1,28 +1,17 @@
 rm(list = ls())
-
-load("hw3_data.RData")
-suppressWarnings()
-
-
-
-
-# EXERCISE 3 --------------------------------------------------------------
-
-# Load packages.
-packs <- c('MASS', 'pracma', 'psych')
+#load packages
+packs <- c('MASS', 'pracma', 'psych','caTools','e1071',"dgof")
 lapply(packs, require, character.only = TRUE)
+###########
 
-
-# Input.
 k <- 5 # Dimensions
-n0 <- 80  # Sample size 0
-n1 <- 90  # Sample size 1
+n0 <- 70  # Sample size 0
+n1 <- 70  # Sample size 1
 alpha <- .05 # significance level
-
-
-# Reproducibility.
-set.seed(324)
-
+set.seed(324)# reproducibility
+P <- 1000
+acc_rej_col <- c("#d3305d" , "#ABCDEF")
+###########
 
 # Take random sample from a multi-normal distribution.
 Take_sample_normal <- function(n, mu, sigma, label){
@@ -34,122 +23,90 @@ Take_sample_normal <- function(n, mu, sigma, label){
 }
 
 
-# Generate k random means.
-generate_mean <- function(k){
-  
-  return(sample(1:10, k))
-}
 
+mu <- rep(0,k)
+sigma <- diag(1 ,k)
 
-# Generate random Covariance matrix (k*k).
-generate_sigma <- function(k){
-  sym_mat <- matrix(1, ncol = k, nrow = k, byrow = TRUE)
-  diag_mat <- diag(sample(0:20), k)
-  sigma <- diag_mat + sym_mat
-  
-  return(sigma)
-}
-
-
-# Sigmoid Function.
-sigmoid <- function(x, theta){
-  n_features <- length(theta)
-  n <- exp(theta[1] + sum(x[-length(x)]*theta[2:n_features]))
-  
-  return(n / (1+n))
-}
-
-?ks.test()
-
-mu <- generate_mean(k)
-sigma <- generate_sigma(k)
-
-x <- Take_sample_normal(n = n0, mu=mu, sigma = sigma, label = 0)
-z <- Take_sample_normal(n = n1, mu=mu, sigma = sigma, label = 1)
-u <- rbind(x, z) # Actual data
+############
 
 
 
-
-# Friedman Procedure ------------------------------------------------------
-
-P <- 1000
-?predict
-Friedman_procedure <- function(P){
-  
+Friedman_procedure <- function(P,xdata,zdata){
   kolm_t <- rep(NA, P)
-
   for(i in 1:P){
     z_p <- Take_sample_normal(n1, mu, sigma, label =1) # Under H_0
-    u_p <- rbind(x, z_p)
-    glm_coef <- unname(glm(label ~ ., data = u_p)$coefficients)
-    x_scores <- apply(x, MARGIN = 1, sigmoid, theta = glm_coef)
-    z_scores <- apply(z_p, MARGIN = 1, sigmoid, theta = glm_coef) 
+    u_p <- rbind(xdata, z_p)
+    glm_model <-glm(label ~ ., data = u_p)
+    x_scores <- predict(glm_model , xdata[,1:k])
+    z_scores <- predict(glm_model,  z_p[,1:k])
     kolm_t[i] <- ks.test(x_scores, z_scores, alternative = "two.sided")$statistic
   }
-  mann_t <<- mann_t
-  kolm_t <<- kolm_t
   
-
+  return(kolm_t)
   
-  hist(kolm_t, main = "
-       Kolmogorov-Smirnov statistic \n distribution under H_0",
-       col = "skyblue", border = "white", breaks= 30)
-  p_kolm <<- quantile(kolm_t , 1 - alpha)
-  abline(v = p_kolm , col = "red" , lty = 3 , lwd = 2)
-  box()
-  
-
 }
+############
 
-Friedman_procedure(P)
-par(mfrow = c(1,1))
-
-# Simulation to get info about ALPHA --------------------------------------
-
-alpha_info <- function(P, percentile_kolm){
-  prop_rej <- rep(NA , P)
+P <- 100
+alpha_info <- function(P){
+  prop_rej <- rep(NA, P)
+  p_values <- rep(NA , P)
+  
   for(i in 1:P){
-    x <- Take_sample_normal(n = n0, mu=mu, sigma = sigma, label = 0)  
-    z <- Take_sample_normal(n = n1, mu = mu, sigma = sigma, label = 1)  # same distributions
-    u <- rbind(x, z) # Combine the data
-    true_coef <- glm(label ~ ., data = u)$coefficients
-    x_scores <- apply(x, MARGIN = 1, sigmoid, theta = true_coef)
-    z_scores <- apply(z, MARGIN = 1, sigmoid, theta = true_coef)
-    true_kolm <- ks.test(x_scores ,z_scores, alternative = "two.sided")$statistic
-    # Perform the Test
-    prop_rej[i] <- true_kolm < percentile_kolm
-
+    x_p <- Take_sample_normal(n = n0, mu = mu, sigma = sigma, label = 0)  
+    z_p <- Take_sample_normal(n = n1, mu = mu, sigma = sigma, label = 1)  # same distributions
+    u_p<- rbind(x_p, z_p) # Combine the data
+    
+    kk <- Friedman_procedure(P,xdata  = x_p , zdata = z) # As mavi said
+    glm_model <-glm(label ~ ., data = u_p)
+    
+    x_scores <- predict(glm_model , x_p[,1:k])
+    z_scores <- predict(glm_model,  z_p[,1:k])
+    
+    true_kolm <- ks.test(x_scores, z_scores, alternative = "two.sided")$statistic
+    prop_rej[i] <- true_kolm < quantile(kk , 1 -alpha)
+    
   }
-
-
   
-  data = as.data.frame(prop_rej)
-  colnames(data) <- c("Kolmogorov-Smirnov")
-  
-  return(data)
+
+  return(prop_rej)
 }
 
 
-data <- alpha_info(P = P, percentile_kolm = p_kolm)
-
-
-barplot(prop.table(table(data$`Kolmogorov-Smirnov`)), col = c("red" , "blue"), main = "Proportion of times we accept-reject \n the null hypothesis  when is actually true \n using KS statistic", names.arg = c("Reject" , "Accept"), ylim = c(0,1))
-
-#barplot(prop.table(table(data$Mann)), col = c("red", "blue"), main = "Proportion of times we accept-reject \n the null hypothesis  when is actually true \n using Mann Statistic", names.arg = c("Reject" , "Accept"), ylim = c(0,1))
+data <- alpha_info(P)
 
 
 
-# Generate x-axis values from 0 to 1
-x <- seq(0, 1, length.out = 1000)
+t <- proportions(table(data[,1]))
+barplot(t, col = acc_rej_col , main = "Proportion of times we accept-reject \n the null hypothesis  when is actually true \n using KS statistic", names.arg = c("Reject" , "Accept"), ylim = c(0,1))
 
-# Plot the Kolmogorov distribution CDF
-plot(x, pkolmogorov(x), type = "l", xlab = "x", ylab = "CDF",
-     main = "Kolmogorov Distribution CDF")
+n0s <- c(20,50,70,100)
+k <- 10
+P <- 150
+mu <- rep(1,k)
+sigma <- diag(1,k)
+alpha_Friedmann <- rep(NA ,length(n0s) )
+for(i in 1:length(n0s)){
+  n0 <- n0s[i]
+  alpha_Friedmann[i] <- 1-mean(alpha_info(P))
 
-# Simulation to get info about POWER --------------------------------------
+}
 
-# Distance functions.
+plot(n0s,alpha_Friedmann,type = "o",ylim = c(0,.4), col = "gold",lwd = 3)
+
+ks <- c(5,20,70,100)
+n0 <- 70
+alpha_Friedmann_2 <- rep(NA ,length(ks))
+P = 150
+for(i in 1:length(ks)){
+  k <- ks[i]
+  mu <- rep(1,k)
+  sigma <- diag(1,k)
+  alpha_Friedmann_2[i] <- 1-mean(alpha_info(P))
+}
+
+plot(ks,alpha_Friedmann_2,type = "o",ylim = c(0,.4), col = "gold",lwd = 3)
+#############
 beta_q <- function(sigma1, sigma2){
   trace_1 <- tr(sigma)
   trace_2 <- tr(sigma2)
@@ -166,65 +123,48 @@ Wasserstein_distance <- function(mu1, mu2, sigma1, sigma2){
 }
 
 
-n0 <- 100
-n1 <- 100
+#############
 
-power_info <- function(P, k, percentile_ks, increment){
-  mu2 <- mu + increment 
-  sigma2 <- sigma
-  distance <- Wasserstein_distance(mu, mu2, sigma, sigma2)
+power_info <- function(P, k, increment){
+  
   prop_ks <- rep(NA, P)
-  prop_mann <- rep(NA, P)
   for(i in 1:P){
     x <- Take_sample_normal(n = n0, mu= mu, sigma = sigma, label = 0)
     z <- Take_sample_normal(n = n1, mu=  mu2, sigma = sigma2, label = 1)
     u <- rbind(x,z) 
-    true_coef <- glm(label ~ ., data = u)$coefficients
-    x_scores <- apply(x, MARGIN = 1, sigmoid, theta = true_coef)
-    z_scores <- apply(z, MARGIN = 1, sigmoid, theta = true_coef)
+    p_model <- glm(label ~ ., data = u)
+    x_scores <- predict(p_model, x)
+    z_scores <- predict(p_model, z) 
     true_kolm <- ks.test(x_scores, z_scores, alternative = "two.sided")$statistic
-    true_mann <- wilcox.test(x_scores, z_scores, alternative = "two.sided")$statistic
-    prop_ks[i] <- true_kolm > percentile_ks
-    prop_mann[i] <- true_mann > percentile_mann
+    kk <- Friedman_procedure(P,x,z)
+    prop_ks[i] <- true_kolm > quantile(kk,1-alpha)
   }
-  data <- c(distance, mean(prop_ks), mean(prop_mann))
-  names(data) <- c('Distance', 'K_S', 'Mann')
+  data <-  mean(prop_ks)
+  
   
   return(data)
 }
 
+
+##########
+P <- 100
 k <- 5
 mu <- 1:k
-sigma <- generate_sigma(length(mu))
-power_info(100, k = length(mu), percentile_ks = p_kolm, percentile_mann  = p_mann, increment = 10)
-
-
-
-
-
-# Plot
-results <- matrix(NA, ncol = 3, nrow = 0)
-
-k <- 10
-P <- 1000
-M <- seq(0,1, .05)
-mu <- generate_mean(k)
-sigma <- generate_sigma(length(mu))
-
-for(l in M){
-  a <- power_info(P, k = length(mu), percentile_ks = p_kolm,
-                  percentile_mann = p_mann, increment = l)
-  results <- rbind(results, a)
-  
+sigma <- diag(1,k)
+max_in <- 1.5
+ll <- seq(from = 0 , to = max_in , by = 0.3 )
+powers <- c()
+distance <- c()
+for (i in (ll)){
+  mu2 <- mu + i 
+  print(mu2)
+  sigma2 <- sigma
+  distance <- c(distance,Wasserstein_distance(mu, mu2, sigma, sigma2))
+  powers <- c(powers,power_info(100, k = length(mu),increment = i))
 }
+plot(distance,powers,col = 'darkblue' , type = "o" , lwd = 2)
 
-install.packages('caTools')
+Wasserstein_distance(mu, mu +1.5 , sigma, sigma)
 
-install.packages('e1071')
-library(e1071)
-?svm
-par(mfrow = c(1,1))
-plot(results$Distance, results$K_S, type = 'l', main='Relation between distance and power', lwd=2, col='skyblue', xlab='Distance', ylab='Power')
-points(results$Distance, results$Mann, type = 'l', main='Relation between distance and power', lwd=2, col='lightgreen')
-legend('bottomright', legend = c('K-S', 'Mann'), col= c('skyblue', 'lightgreen'), lwd=3)
-
+#######
+#######
